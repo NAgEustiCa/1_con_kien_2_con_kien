05f615b94bb0f8e28c1bcd9a60e02f5d970d5fb3
NAgEustiCa <112616523+NAgEustiCa@users.noreply.github.com>
2022-10-21 02:43:37 +0000

    cn

---
+++ coco.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         CoCo
+// @namespace    https://github.com/NAgEustiCa/1_con_kien_2_con_kien/raw/main/coco.user.js
+// @version      2022-Oct-21
+// @description  download coco
+// @author       You
+// @match        https://www.cocomanga.com/*/*/*.html
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=cocomanga.com
+// @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
+// @grant        none
+// @downloadURL  https://github.com/NAgEustiCa/1_con_kien_2_con_kien/raw/main/coco.user.js
+// @updateURL    https://github.com/NAgEustiCa/1_con_kien_2_con_kien/raw/main/coco.meta.js
+// ==/UserScript==

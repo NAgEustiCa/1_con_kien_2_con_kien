5c5af6905b6cf8a302f161c7e06e6c719207fe41
NAgEustiCa <112616523+NAgEustiCa@users.noreply.github.com>
2023-01-09 01:09:22 +0000

    cn

--- coco.meta.js
+++ coco.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CoCo
 // @namespace    https://github.com/NAgEustiCa/1_con_kien_2_con_kien/raw/main/coco.user.js
-// @version      2023.01.08
+// @version      2023.01.09
 // @description  download coco
 // @author       You
 // @match        https://www.colamanhua.com/*/*/*.html

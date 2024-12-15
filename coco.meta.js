fa7953d196283259b7714db04f2e5e39aaafa572
NAgEustiCa <112616523+NAgEustiCa@users.noreply.github.com>
2024-12-15 16:21:09 +0700

    up

--- coco.meta.js
+++ coco.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CoCo
 // @namespace    https://github.com/NAgEustiCa/1_con_kien_2_con_kien/raw/main/coco.user.js
-// @version      2024.12.16
+// @version      2024.12.16.1
 // @description  download coco
 // @author       You
 // @match        https://www.colamanga.com/*/*/*.html

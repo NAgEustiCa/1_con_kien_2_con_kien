275d531d6338ff0416a050bab21f3574ec355dd3
NAgEustiCa <112616523+NAgEustiCa@users.noreply.github.com>
2024-12-15 16:17:17 +0700

    init

---
+++ coco.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         CoCo
+// @namespace    https://github.com/NAgEustiCa/1_con_kien_2_con_kien/raw/main/coco.user.js
+// @version      2024.12.16
+// @description  download coco
+// @author       You
+// @match        https://www.colamanga.com/*/*/*.html
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=colamanga.com
+// @require      https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.7.53/dist/zip.min.js
+// @grant        none
+// @run-at       document-start
+// @downloadURL  https://github.com/NAgEustiCa/1_con_kien_2_con_kien/raw/main/coco.user.js
+// @updateURL    https://github.com/NAgEustiCa/1_con_kien_2_con_kien/raw/main/coco.meta.js
+// ==/UserScript==
